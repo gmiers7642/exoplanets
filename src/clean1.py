72d1a53ae5a72cfe6ca79fa53d3de91e0f2a655f
glenn <glenn@glenns-MacBook-Pro.local>
2016-10-24 17:42:01 -0500

    Added extra columns to the data analysis, and all functions now work with it, except for physical_scatterplots()

--- src/clean1.py
+++ src/clean1.py
@@ -2,18 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def get_physical_columns(df):
-    cols_phys = ['pl_orbper', 'pl_orbsmax', 'pl_orbeccen', 'pl_orbincl',
-                 'pl_bmassj', 'pl_radj', 'pl_dens', 'st_dist',
-                 'st_optmag', 'st_teff', 'st_mass', 'st_rad']
-    return df[cols_phys]
+def get_physical_columns(df, cols):
+    return df[cols]
 
-def plot_physical_histograms(df, logcols):
+def plot_physical_histograms(df, logcols, num_rows, num_cols):
     df = df.copy()
     plt.figure(figsize=(20,20))
     for col in df.columns:
         color = 'blue'
-        ax = plt.subplot(4, 3, list(df.columns).index(col)+1)
+        ax = plt.subplot(num_rows, num_cols, list(df.columns).index(col)+1)
         if col in logcols:
             temp = df.loc[:,col].apply(lambda x: np.log(x+1)).dropna()
             color = 'red'
@@ -22,8 +19,7 @@ def plot_physical_histograms(df, logcols):
             temp = df.loc[:,col].dropna()
             label = col
         ax.hist(temp, bins=35, color=color)
-        ax.set_title(label, fontsize=20)
-
+        ax.set_title(label, fontsize=10)
     plt.tight_layout()
     plt.show()
 
@@ -34,8 +30,8 @@ def physical_scatterplot(df, logcols):
     plt.figure(figsize=(12,12))
     for col in df.columns:
         if col in logcols:
-            df.loc[:,col] = df.loc[:,col].apply(lambda x: np.log(x+1)).dropna()
-    sns.pairplot(data=df.fillna(value=0))
+            df.loc[:,col] = df.loc[:,col].apply(lambda x: np.log(x+1)).fillna(value=0.)
+    sns.pairplot(data=df)
     plt.show()
 
 def convert_to_polar(df):
@@ -50,17 +46,26 @@ def convert_to_polar(df):
 def plot_XYZ_histograms(df):
     #fig = plt.figure(figsize=(20,8))
     df_c.hist(bins=25)
-    plt.suptitle("XYZ Coordinate Convervion Check")
+    plt.suptitle("XYZ Coordinate Conversion Check")
     plt.tight_layout()
     plt.show()
 
 if __name__ == '__main__':
-    # Extract columns
     df = pd.read_csv('../data/planets.csv')
-    df_p = get_physical_columns(df)
+
+    # Extract columns
+    cols_phys = ['pl_orbper', 'pl_orbsmax', 'pl_orbeccen', 'pl_orbincl',
+                 'pl_bmassj', 'pl_radj', 'pl_dens', 'st_dist',
+                 'st_optmag', 'st_teff', 'st_mass', 'st_rad',
+                 'st_logg', 'st_dens', 'st_lum', 'pl_rvamp',
+                 'pl_eqt', 'st_plx', 'st_age', 'st_vsini',
+                 'st_acts']
+    df_p = get_physical_columns(df, cols_phys)
+
     logcols = ['pl_bmassj', 'pl_dens', 'pl_orbper', 'pl_orbsmax',
-               'pl_radj', 'st_dist', 'st_rad', 'st_teff']
-    #plot_physical_histograms(df_p, logcols)
+               'pl_radj', 'st_dist', 'st_rad', 'st_teff', 'st_dens',
+               'pl_rvamp', 'st_plx', 'st_vsini', 'st_acts']
+    #plot_physical_histograms(df_p, logcols, 5, 5)
 
     # Convert from polar to XYZ
     '''df_c = convert_to_polar(df)
