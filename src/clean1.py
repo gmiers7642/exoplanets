266b915225e2d22161aaa1ae64ff9bb222a4de08
glenn <glenn@glenns-MacBook-Pro.local>
2016-10-25 14:59:01 -0500

    Refactoring and tests

--- src/clean1.py
+++ src/clean1.py
@@ -2,8 +2,28 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+################################################################################
+# Cleaning Functions
+################################################################################
+
+def import_data(filename):
+    return pd.read_csv(filename)
+
 def get_physical_columns(df, cols):
     return df[cols]
+
+def convert_to_polar(df):
+    # Reference:
+    # http://math.stackexchange.com/questions/15323/how-do-i-calculate-the-cartesian-coordinates-of-stars
+    df_c = df.loc[:,['ra', 'dec', 'st_dist']]
+    df_c['X'] = ( df_c['st_dist'] * np.cos(df_c['dec']) ) * np.cos(df_c['ra'])
+    df_c['Y'] = ( df_c['st_dist'] * np.cos(df_c['dec']) ) * np.sin(df_c['ra'])
+    df_c['Z'] = df_c['st_dist'] * np.sin(df_c['dec'])
+    return df_c
+
+################################################################################
+# Plotting Functions
+################################################################################
 
 def plot_physical_histograms(df, logcols, num_rows, num_cols):
     df = df.copy()
@@ -39,23 +59,40 @@ def physical_scatterplot(df, hcols, vcols, logcols):
     plt.tight_layout()
     plt.show()
 
-def convert_to_polar(df):
-    # Reference:
-    # http://math.stackexchange.com/questions/15323/how-do-i-calculate-the-cartesian-coordinates-of-stars
-    df_c = df.loc[:,['ra', 'dec', 'st_dist']]
-    df_c['X'] = ( df_c['st_dist'] * np.cos(df_c['dec']) ) * np.cos(df_c['ra'])
-    df_c['Y'] = ( df_c['st_dist'] * np.cos(df_c['dec']) ) * np.sin(df_c['ra'])
-    df_c['Z'] = df_c['st_dist'] * np.sin(df_c['dec'])
-    return df_c
-
 def plot_XYZ_histograms(df):
     #fig = plt.figure(figsize=(20,8))
-    df_c.hist(bins=25)
+    df.hist(bins=25)
     plt.suptitle("XYZ Coordinate Conversion Check")
     plt.tight_layout()
     plt.show()
 
-if __name__ == '__main__':
+################################################################################
+# Unit tests
+################################################################################
+
+def unit001():
+    df = import_data('../data/planets.csv')
+
+    # Extract columns
+    cols_phys = ['pl_orbper', 'pl_orbsmax', 'pl_orbeccen', 'pl_orbincl',
+                 'pl_bmassj', 'pl_radj', 'pl_dens', 'st_dist',
+                 'st_optmag', 'st_teff', 'st_mass', 'st_rad',
+                 'st_logg', 'st_dens', 'st_lum', 'pl_rvamp',
+                 'pl_eqt', 'st_plx', 'st_age', 'st_vsini',
+                 'st_acts']
+    df_p = get_physical_columns(df, cols_phys)
+
+    print "Physical data, first few rows:"
+    print df_p.head()
+
+def unit002():
+    df = import_data('../data/planets.csv')
+
+    # Convert from polar to XYZ
+    df_c = convert_to_polar(df)
+    plot_XYZ_histograms(df_c)
+
+def unit003():
     df = pd.read_csv('../data/planets.csv')
 
     # Extract columns
@@ -70,13 +107,13 @@ if __name__ == '__main__':
     logcols = ['pl_bmassj', 'pl_dens', 'pl_orbper', 'pl_orbsmax',
                'pl_radj', 'st_dist', 'st_rad', 'st_teff', 'st_dens',
                'pl_rvamp', 'st_plx', 'st_vsini', 'st_acts']
-    #plot_physical_histograms(df_p, logcols, 5, 5)
 
-    # Convert from polar to XYZ
-    '''df_c = convert_to_polar(df)
-    plot_XYZ_histograms(df_c)'''
-
-    # Scatterplot matrix for physical data
+    # Scatterplot matrix for physical data, for one selected quadrant
     cph1 = cols_phys[0:10]
     cph2 = cols_phys[11:]
     physical_scatterplot(df_p, cph1, cph2, logcols)
+
+if __name__ == '__main__':
+    #unit001()
+    #unit002()
+    #unit003()
