36e39e0348880de140d566265c520c8435610295
glenn <glenn@glenns-MacBook-Pro.local>
2016-11-02 14:49:02 -0500

    Added agg_clustering() and color_coded_pc_plot() functions with tests

--- src/clustering.py
+++ src/clustering.py
@@ -61,6 +61,12 @@ def kmeans_centroid_fill(df, n_clusters, max_iterations):
     # Return the labels output by KMeans
     return clus.labels_, Xm
 
+def agg_clustering(df, cols, n_clusters):
+    df_select = df[cols]
+    ac = AgglomerativeClustering(n_clusters=n_clusters)
+    ac.fit(df_select)
+    return ac, df_select
+
 ################################################################################
 # Plotting Functions
 ################################################################################
@@ -86,6 +92,14 @@ def plot_pc_features(vt, num, cols, title=""):
         ax.bar(left=np.arange(len(cols)), height=vt[:,i])
         ax.set_xticklabels(cols, rotation='vertical')
     plt.suptitle("Principal component plot for " + title)
+    plt.show()
+
+def color_coded_pc_plot(df, labels, xlim=(-0.021, -0.01), ylim=(-0.07, 0.03)):
+    u,s,vt = svd(df)
+    fig = plt.figure(figsize=(12,12))
+    plt.scatter(u[:,0], u[:,1], c=labels, s=45, alpha=0.1)
+    plt.xlim(xlim)
+    plt.ylim(ylim)
     plt.show()
 
 ################################################################################
@@ -182,6 +196,39 @@ def unit003():
     print "Creating feature plots"
     plot_pc_features(vt, 4, df_imputed.columns, "21 columns")
 
+# To test plotting of color coded pcs
+def unit004():
+    print "Importing data..."
+    df = c.import_data('../data/planets.csv')
+
+    # Extract columns
+    cols_phys = ['pl_orbper', 'pl_orbsmax', 'pl_orbeccen', 'pl_orbincl',
+                 'pl_bmassj', 'pl_radj', 'pl_dens', 'st_dist',
+                 'st_optmag', 'st_teff', 'st_mass', 'st_rad',
+                 'st_logg', 'st_dens', 'st_lum', 'pl_rvamp',
+                 'pl_eqt', 'st_plx', 'st_age', 'st_vsini',
+                 'st_acts']
+    df_p = c.get_physical_columns(df, cols_phys)
+
+    logcols = ['pl_bmassj', 'pl_dens', 'pl_orbper', 'pl_orbsmax',
+               'pl_radj', 'st_dist', 'st_rad', 'st_teff', 'st_dens',
+               'pl_rvamp', 'st_plx', 'st_vsini', 'st_acts']
+
+    # Pre-process the data, imputation, and create svd for plotting
+    print "Applying pre-processing..."
+    km_labels, df_imputed = kmeans_centroid_fill(df_p, 3, 10)
+
+    # Columns selected from previous analysi of 21 columns
+    print "Clustering..."
+    cols = ['st_age', 'pl_orbsmax', 'pl_bmassj', 'st_plx', 'st_dist', 'st_lum', 'st_mass']
+    n_clusters = 3
+    ac, df_select = agg_clustering(df_imputed, cols, n_clusters)
+    labels = ac.labels_
+
+    # Create the color codeded pc plot
+    print "Creating pc plot..."
+    color_coded_pc_plot(df_select, labels)
+
 if __name__ == '__main__':
     # unit001 - To test Kmeans centroid clustering workflwo on full 21 features
     #unit001()
@@ -190,4 +237,7 @@ if __name__ == '__main__':
     #unit002()
 
     # unit003 - To test plotting of columns with the most relevance from an svd
-    unit003()
+    #unit003()
+
+    # unit004 - To test plotting of color coded pcs
+    unit004()
