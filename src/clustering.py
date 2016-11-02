7ec7e450872a67b21abae8390287d1bcd317a88d
glenn <glenn@glenns-MacBook-Pro.local>
2016-11-02 14:29:26 -0500

    Added plot_pc_features() for determining feature importance

--- src/clustering.py
+++ src/clustering.py
@@ -79,6 +79,15 @@ def plot_pcs(A, rows, cols, T=False, colors=['blue']):
     plt.suptitle("Principal Components Plot", fontsize=15)
     plt.show()
 
+def plot_pc_features(vt, num, cols, title=""):
+    fig = plt.figure(figsize=(20,8))
+    for i in range(num):
+        ax = plt.subplot(1,4,i+1)
+        ax.bar(left=np.arange(len(cols)), height=vt[:,i])
+        ax.set_xticklabels(cols, rotation='vertical')
+    plt.suptitle("Principal component plot for " + title)
+    plt.show()
+
 ################################################################################
 # Unit tests
 ################################################################################
@@ -146,9 +155,39 @@ def unit002():
     c.physical_scatterplot(df_p, cph1, cph2, logcols=df_p.columns, \
                                 colors=ac_labels, alpha=0.2)
 
+# To test plotting of columns with the most relevance from an svd
+def unit003():
+    print "Importing data..."
+    df = c.import_data('../data/planets.csv')
+
+    # Extract columns
+    cols_phys = ['pl_orbper', 'pl_orbsmax', 'pl_orbeccen', 'pl_orbincl',
+                 'pl_bmassj', 'pl_radj', 'pl_dens', 'st_dist',
+                 'st_optmag', 'st_teff', 'st_mass', 'st_rad',
+                 'st_logg', 'st_dens', 'st_lum', 'pl_rvamp',
+                 'pl_eqt', 'st_plx', 'st_age', 'st_vsini',
+                 'st_acts']
+    df_p = c.get_physical_columns(df, cols_phys)
+
+    logcols = ['pl_bmassj', 'pl_dens', 'pl_orbper', 'pl_orbsmax',
+               'pl_radj', 'st_dist', 'st_rad', 'st_teff', 'st_dens',
+               'pl_rvamp', 'st_plx', 'st_vsini', 'st_acts']
+
+    # Pre-process the data, imputation, and create svd for plotting
+    print "Applying pre-processing"
+    km_labels, df_imputed = kmeans_centroid_fill(df_p, 3, 10)
+    u,s,vt = svd(df_imputed)
+
+    # Create plot of most important components
+    print "Creating feature plots"
+    plot_pc_features(vt, 4, df_imputed.columns, "21 columns")
+
 if __name__ == '__main__':
     # unit001 - To test Kmeans centroid clustering workflwo on full 21 features
-    unit001()
+    #unit001()
 
     # unit002 - To test Aglorrerative clustering workflwo on full 21 features (non-imputed data)
     #unit002()
+
+    # unit003 - To test plotting of columns with the most relevance from an svd
+    unit003()
